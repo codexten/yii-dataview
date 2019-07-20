3693ed961234cdf973b3906602cbc07aa6fdd2e9
Jomon Johnson <jomon.entero@gmail.com>
2019-07-20 16:41:47 +0530

    init

---
+++ src/grid/export/ExportMenu.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace codexten\yii\dataView\grid\export;
+
+class ExportMenu extends \kartik\export\ExportMenu
+{
+
+}
--- src/widgets/GridView.php
+++ src/widgets/GridView.php
@@ -4,5 +4,15 @@ namespace codexten\yii\dataView\widgets;
 
 class GridView extends \hiqdev\higrid\GridView
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function init()
+    {
+        if (empty($this->columns)) {
+            $this->columns = $this->columns();
+        }
+        parent::init();
+    }
 
 }
